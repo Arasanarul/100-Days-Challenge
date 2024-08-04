988df0d69cc38c650f5be95f5112071671ea2d22
Arularasan Sivakumar <159014383+Arasanarul@users.noreply.github.com>
2024-08-04 23:11:50 +0530

    Update day_4.java

--- day_4.java
+++ day_4.java
@@ -1,1 +1,26 @@
 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++){
+		    int a=sc.nextInt();
+		    int b=sc.nextInt();
+		    int c=0;
+		    if(a>=b)
+		    c=a-b;
+		    
+		    int ans=(c/4);
+		    ans=(c%4!=0)?ans+1:ans;
+		    System.out.println(ans);
+		}
+
+	}
+}
