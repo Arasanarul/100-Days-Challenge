b248006063cdd77905a4dc37b400671b57758b40
Arularasan Sivakumar <159014383+Arasanarul@users.noreply.github.com>
2024-08-07 23:23:49 +0530

    Update day_7.java

--- day_7.java
+++ day_7.java
@@ -1,1 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+    int x = 111;
+    while (111>=2){
+      System.out.print(x-- + " ");
+    }
+  } 
