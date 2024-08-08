5749946050ced099d237a63ae6d152ad5c65bf0b
Arularasan Sivakumar <159014383+Arasanarul@users.noreply.github.com>
2024-08-08 23:06:41 +0530

    Create day_8.java

---
+++ day_8.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+	  Scanner sc = new Scanner(System.in);
+    Float x = sc.nextFloat();
+    System.out.printf("%.2f",x*0.8);  
+	}
+}
