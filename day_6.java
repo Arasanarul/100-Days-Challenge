aacf13fd5e8df3cd148f3b5b7a1eb1a3a7a31c3c
Arularasan Sivakumar <159014383+Arasanarul@users.noreply.github.com>
2024-08-12 21:33:59 +0530

    Update day_6.java

--- day_6.java
+++ day_6.java
@@ -1,1 +1,16 @@
+import java.util.*;
+public class Hello {
 
+    public static void main(String[]args) {
+    Scanner sc = new Scanner(System.in);
+    int x = sc.nextInt();
+    int y = sc.nextInt();
+    if(x%2==0 && y%2==0){
+        System.out.print("EVEN");
+    }else if(x%2==1 && y%2==1){
+        System.out.print("ODD");
+    }else{
+        System.out.print("MIXWD");
+    }
+    }
+}
