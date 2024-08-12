091052320592cd818264d1498371e7c6eebf41ac
Arularasan Sivakumar <159014383+Arasanarul@users.noreply.github.com>
2024-08-12 20:41:09 +0530

    Update day_5.java

--- day_5.java
+++ day_5.java
@@ -1,1 +1,10 @@
-//
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        for(int i=x; i<=222; i++){
+        System.out.println(i);
+        }
+    }
+}
