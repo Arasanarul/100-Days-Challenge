d592b0fc60a678a068696311a8d0de5681f1a5cc
Arularasan Sivakumar <159014383+Arasanarul@users.noreply.github.com>
2024-08-13 22:07:34 +0530

    Update day_11.java

--- day_11.java
+++ day_11.java
@@ -1,1 +1,12 @@
-//greenwalk
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int x = sc.nextInt();
+	for(int i=x;i<x+x;i++){
+	    System.out.print(i + " ");
+	}
+
+	}
+}
