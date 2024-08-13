e5d10e1692e64f93e02c04d3ec392f8379f9a32e
Arularasan Sivakumar <159014383+Arasanarul@users.noreply.github.com>
2024-08-13 22:56:03 +0530

    Update day_13.java

--- day_13.java
+++ day_13.java
@@ -1,1 +1,15 @@
-gfxhgfn 
+import java.util.*;
+public class Hello {
+    
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	String x = sc.next();
+	String y = sc.next();
+   if(x.charAt(0)==y.charAt(0) && x.charAt(1)==y.charAt(1) && x.charAt(2)==y.charAt(2)){
+	       System.out.print("YES");
+	   }else{
+	    System.out.print("NO");
+	}
+
+	}
+}
