5cc0f16d9b4ff5d391e6b062a9baa801100beb26
Arularasan Sivakumar <159014383+Arasanarul@users.noreply.github.com>
2024-08-13 21:11:24 +0530

    Update day_9.java

--- day_9.java
+++ day_9.java
@@ -1,1 +1,16 @@
-hiiiiiiiiiiiiiiiiiiiiii
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int x = sc.nextInt();
+	int y = sc.nextInt();
+	int z = sc.nextInt();
+	if(x+y==z || x+z==y){
+	    System.out.print("yes");
+	}else{
+	    System.out.print("no");
+	}
+
+	}
+}
