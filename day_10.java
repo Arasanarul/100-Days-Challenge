9d2dfd684f757cb6fb8cc6372a3def88e4505b2e
Arularasan Sivakumar <159014383+Arasanarul@users.noreply.github.com>
2024-08-13 21:45:01 +0530

    Update day_10.java

--- day_10.java
+++ day_10.java
@@ -1,1 +1,14 @@
+import java.util.*;
+public class Hello {
 
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int x = sc.nextInt();
+	if (x%30==0 ||x%30==1 || x%30==2){
+	    System.out.print("YES");
+	}else{
+	    System.out.print("NO");
+	}
+
+	}
+}
