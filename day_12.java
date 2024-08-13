b963b50ae42b2c8114cf5c521f8174f6550ca99f
Arularasan Sivakumar <159014383+Arasanarul@users.noreply.github.com>
2024-08-13 22:19:51 +0530

    Update day_12.java

--- day_12.java
+++ day_12.java
@@ -1,1 +1,16 @@
-///java
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int x = sc.nextInt();
+	int y = sc.nextInt();
+	int z = sc.nextInt();
+	if((x>=y)&&(y>=z) || (x<=y)&&(y<=z)){
+	    System.out.print("SORTED");
+	}else{
+	    System.out.print("NOTSORTED");
+	}
+
+	}
+}
