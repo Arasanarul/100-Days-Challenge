373cacbf4ef8c2b09617f6d39d2cba95770b0731
Arularasan Sivakumar <159014383+Arasanarul@users.noreply.github.com>
2024-08-15 12:53:09 +0530

    Update day_15.java

--- day_15.java
+++ day_15.java
@@ -12,3 +12,34 @@ public class Hello {
     }
 	}
 }
+
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int x = sc.nextInt();
+	int y = sc.nextInt();
+	int z = sc.nextInt();
+	if(y<x && y<z){
+	    if(x%y==0 && z%y==0){
+	        System.out.print("yes");
+	    }else{
+	        System.out.print("no");
+	    }
+	}else if(x<y && x<z){
+	    if(y%x==0 && z%x==0){
+	        System.out.print("yes");
+	    }else{
+	        System.out.print("no");
+	    }
+	}else{
+	    if(x%z==0 && y%z==0){
+	        System.out.print("yes");
+	    }else{
+	        System.out.print("no");
+	    }
+	}
+	
+	}
+}
