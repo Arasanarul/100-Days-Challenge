f5e15f388294b6b975c783f04a67be1c92df280e
Arularasan Sivakumar <159014383+Arasanarul@users.noreply.github.com>
2024-08-17 22:14:58 +0530

    Create day_17.java

---
+++ day_17.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	char x = sc.next().charAt(0);
+	for(int i = 1;i<=5; i++ ){
+	    System.out.print(x);
+	    x++;
+	}
+
+	}
+}
