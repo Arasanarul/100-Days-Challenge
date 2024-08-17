b30b05a71035e945a3c04b3613973adc7edd39ff
Arularasan Sivakumar <159014383+Arasanarul@users.noreply.github.com>
2024-08-17 22:37:40 +0530

    Update day_17.java

--- day_17.java
+++ day_17.java
@@ -11,3 +11,21 @@ public class Hello {
 
 	}
 }
+
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int x = sc.nextInt();
+	int y = 0,z = 1;
+	while( y < 3){
+	    if(x % z==0){
+	        System.out.print(z + " ");
+	        y++;
+	    }
+	    z++;
+	}
+	}
+}
+
