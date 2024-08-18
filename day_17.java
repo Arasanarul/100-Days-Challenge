8209d1207f9c0f086a679223a3cc230192fe084a
Arularasan Sivakumar <159014383+Arasanarul@users.noreply.github.com>
2024-08-18 21:27:10 +0530

    Update day_17.java

--- day_17.java
+++ day_17.java
@@ -18,14 +18,15 @@ public class Hello {
     public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
 	int x = sc.nextInt();
-	int y = 0,z = 1;
-	while( y < 3){
-	    if(x % z==0){
-	        System.out.print(z + " ");
-	        y++;
+	int y=0;
+	for(int i = 1; i<=x ;i++){
+	    if(x%i==0){
+	        System.out.print(i+ " ");y+=1;
 	    }
-	    z++;
+	    if(y==3) {
+	    break;
+	    }
 	}
-	}
+}
 }
 
