7577480caed4d4baf44987e75bb77bc4338d58ca
Arularasan Sivakumar <159014383+Arasanarul@users.noreply.github.com>
2024-08-19 21:39:55 +0530

    Update day_19.java

--- day_19.java
+++ day_19.java
@@ -1,1 +1,16 @@
-afsaegv
+import java.util.Scanner;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        if (N < 10) {
+            int M = N+1;
+            System.out.print(M);
+        } else {
+            int O = N-1;
+            System.out.print(O);
+        }
+    }
+}
