5ef9c7467069224897671612bfcd4ad896f68051
Arularasan Sivakumar <159014383+Arasanarul@users.noreply.github.com>
2024-08-22 22:26:50 +0530

    Update day_21.java

--- day_21.java
+++ day_21.java
@@ -1,1 +1,15 @@
-gcd
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int x = sc.nextInt();
+	int y = sc.nextInt();
+	int z = 0;
+	for(int i=1;i<=x;i++){
+	    if(x%i==0 && y%i==0)
+	    z=i;
+	} System.out.print(z);
+
+	}
+}
