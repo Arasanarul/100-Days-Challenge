ff31d0d743f7232535ca055fa71082e4a6d44583
Arularasan Sivakumar <159014383+Arasanarul@users.noreply.github.com>
2024-08-27 20:57:29 +0530

    Update day_22.java

--- day_22.java
+++ day_22.java
@@ -1,1 +1,15 @@
-process
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	String x = sc.next();
+	for(int i = 0;i<=4; i++ ){
+	    for(int j=1; j<=i+1; j++){
+	    System.out.print(x.charAt(i));
+	    
+	}
+
+	}
+}
+}
