c906ee05feac6239aebf2218fca4fdfea9ba679e
Arularasan Sivakumar <159014383+Arasanarul@users.noreply.github.com>
2024-08-28 22:24:04 +0530

    Update day_24.java

--- day_24.java
+++ day_24.java
@@ -1,1 +1,13 @@
+import java.util.*;
+public class Hello {
 
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int x = 5;
+	int y = 10;
+	x=x+y;
+	y=x-y;
+	x=x-y;
+	System.out.print(x);
+	System.out.print(y);
+}}
