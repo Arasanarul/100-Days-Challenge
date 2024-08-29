601554eb91c02472f8c3266b083a2a6ad9cf515c
Arularasan Sivakumar <159014383+Arasanarul@users.noreply.github.com>
2024-08-29 22:21:02 +0530

    Update day_27.java

--- day_27.java
+++ day_27.java
@@ -1,1 +1,16 @@
+import java.util.*;
+public class Hello {
 
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int x = sc.nextInt();
+    int y=0;
+    int z=1;
+    for(int i = 1; i<=x; i++){
+        y=y+z;
+        z =y;
+        y=z;
+    System.out.print(z + " ");
+}
+}
+}
