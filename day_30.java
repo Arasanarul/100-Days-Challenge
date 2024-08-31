79153b59d7671b43e5a656c7545040be8c4209f1
Arularasan Sivakumar <159014383+Arasanarul@users.noreply.github.com>
2024-08-31 23:36:28 +0530

    Update day_30.java

--- day_30.java
+++ day_30.java
@@ -1,1 +1,21 @@
-array
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int n=sc.nextInt();
+	int[] ar=new int[n];
+	for(int i=0; i<n;i++){
+	    
+	ar[i]=sc.nextInt();
+	int a=ar[i];
+	int b=0;
+    while(a>0){
+        b=b*10+(a%10);
+        a/=10;
+}
+	    ar[i]=b;
+	}
+	System.out.println(Arrays.toString(ar));
+}
+}
