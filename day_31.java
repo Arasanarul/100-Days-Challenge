9c26dd4de14550ba7b8787f4f903504a3acfa48c
Arularasan Sivakumar <159014383+Arasanarul@users.noreply.github.com>
2024-08-31 22:18:40 +0530

    Create day_31.java

---
+++ day_31.java
@@ -0,0 +1,18 @@
+//int a=10; char ch='a'; String st="Arul";
+//int ar[]=new int[5];
+//ar={1,23,4,5,6};
+//ar[0]=1;
+//ar[1]=23;
+//int[] ar={1,2,3,4,5,6,7,8,9};
+//for(int i=0;i<9;i++){
+//sop(ar[i]+" ");
+//}
+//ar[i]=sc.nextInt();
+//
+//
+//
+//
+//
+//
+//
+//
