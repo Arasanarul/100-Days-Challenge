ce23d5759535f9407194915c3cb3404999596d0e
Arularasan Sivakumar <159014383+Arasanarul@users.noreply.github.com>
2024-09-02 20:18:18 +0530

    Update day_32.java

--- day_32.java
+++ day_32.java
@@ -1,2 +1,19 @@
-123
-  
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+        int a=1;
+        
+        while(b>0){
+            int c = (b%10);
+            b/=10;
+            
+            
+        System.out.print(c);
+        }
+	
+
+	}
+}
